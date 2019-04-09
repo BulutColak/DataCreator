4e63d867b2c062ce735612e520554422abfbfb92
bulutcolak94 <bulutcolak94@gmail.com>
2019-04-09 20:54:33 +0300

    Oluştur'a basıldığında Excel'i açıyor

--- DataCreator/Form1.cs
+++ DataCreator/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace DataCreator
 {
@@ -20,6 +21,20 @@ namespace DataCreator
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            Excel.Application xlApp = new Excel.Application();
+            xlApp.Visible = true;
+            xlApp.DisplayAlerts = true;
+
+            Excel.Workbook wb = xlApp.Workbooks.Add(Excel.XlWBATemplate.xlWBATWorksheet);
+            Excel.Worksheet ws = (Excel.Worksheet)wb.Sheets[1];
+
+            ws.Cells[1, 1] = "Merhaba - Hello";
         }
     }
 }
